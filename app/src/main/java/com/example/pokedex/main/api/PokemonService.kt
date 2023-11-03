123d95e1229cab0610ff61737e1a9224d96ce04b
let1ci4b <leticia_b_souza@estudante.sc.senai.br>
2023-11-03 16:25:29 -0300

    card view size replaced

--- app/src/main/java/com/example/pokedex/main/api/PokemonService.kt
+++ app/src/main/java/com/example/pokedex/main/api/PokemonService.kt
@@ -1,12 +1,11 @@
 package com.example.pokedex.main.api
 
 import com.example.pokedex.main.dto.PokemonResponseDTO
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface PokemonService {
     @GET("pokemon/{id}/")
-    suspend fun getSinglePokemon( @Path("id") id: Int? ) : Response<PokemonResponseDTO>
+    suspend fun getSinglePokemon( @Path("id") id: Int? ) : PokemonResponseDTO
 
 }
